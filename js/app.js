a167a937acd8ba43143953fd553a799e0a1751d7
yulsep <yulimarcela99@gmail.com>
2022-09-21 15:54:32 -0500

    Contadores de 0 a 10 y de 0 al input

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Voy a contar del 0 al 9
+
+for (let index = 0; index <= 10; index++) {
+    console.log("Estamos contando el numero es: "+index)    
+}
+
+let myContador = parseInt(prompt("Contar hasta cuanto?"))
+for (let index = 0; index <= myContador; index++) {
+    console.log("Estamos contando el numero es: "+index)    
+}
